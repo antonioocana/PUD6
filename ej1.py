d73a11ad7a9cabb2a4693f232763d5eaeb8eba1b
Antonio <antonio@example.com>
2023-03-06 13:18:16 +0100

    Revert "metido un condicional"
    This reverts commit d3637b04ce365d5d7005a3033a943d18bda9bd8c.

--- ej1.py
+++ ej1.py
@@ -1,17 +1,10 @@
 nombre = input("introduce tu nombre: ")
-valor = 8
 # comentario 1 linea
 
 '''
     Comentario lo que sea
 '''
 def hola(nombre_recibido):
-    if valor < 4:
-        print("Hola")
-    elif valor > 4 and valor < 9:
-        print("Adios")
-    else:
-        print("Buenos dias")
     print("Hola mundo " + nombre_recibido)
     return 1
 
