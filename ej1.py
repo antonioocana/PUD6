fa5ccd7cff8988ce79a615634367357af12d069c
Antonio <antonio@example.com>
2023-03-06 12:55:19 +0100

    Explicando funciones

--- ej1.py
+++ ej1.py
@@ -1,3 +1,11 @@
 nombre = input("introduce tu nombre: ")
+# comentario 1 linea
 
-print("Hola mundo " + nombre)
+'''
+    Comentario multiples lineas
+'''
+def hola(nombre_recibido):
+    print("Hola mundo " + nombre_recibido)
+    return 1
+
+hola(nombre)
