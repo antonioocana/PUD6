ce743fc6016817b2f1631c95fd282014d04bec29
Antonio <antonio@example.com>
2023-03-06 13:02:50 +0100

    Preparando conflicto en develop

--- ej1.py
+++ ej1.py
@@ -2,7 +2,7 @@ nombre = input("introduce tu nombre: ")
 # comentario 1 linea
 
 '''
-    Comentario multiples lineas
+    Comentario lo que sea
 '''
 def hola(nombre_recibido):
     print("Hola mundo " + nombre_recibido)
